8444a5bd98afa26a51b277d01a1578cc16fe58a0
79969 <m.byckoff2015@yandex.ru>
2023-10-01 18:25:32 +0300

    init: initial commit

---
+++ src/main/java/com/bykov/jobilee/JobileeApplication.java
@@ -0,0 +1,13 @@
+package com.bykov.jobilee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobileeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobileeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bykov/jobilee/JobileeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bykov.jobilee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobileeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
